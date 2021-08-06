ca451f99acea6c76220ddc217ef932338a4e7429
Mai Lam <ltxmai221@gmail.com>
2021-08-06 17:18:12 +0700

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+
+// SMOOTHSCROLL
+    const scroll = new SmoothScroll('.header__navbar a[href*="#"]', {
+        speed: 800
+    });
+
+// LOADING 
+    $(window).load(function() {
+        $('.loading').delay(500).fadeOut('fast', function() {
+            $('body').removeClass('preloading');
+        });
+    });
+
+
+// LOADING PAGE
+    window.addEventListener('scroll', reveal);
+    function reveal() {
+        var reveals = document.querySelectorAll('.reveal');
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var revealTop = reveals[i].getBoundingClientRect().top;
+            var revealPoint = 100;
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active');
+            }
+            else {
+                reveals[i].classList.remove('active');
+            }
+        }
+    }
+
+
+// refresh to top 
+    window.onbeforeunload = function() {
+        window.scrollTo(0, 0);
+    }; 
+
+
+// NAVBAR RESPONSIVE
+    const navSlide = () => {
+        const bars = document.querySelector('.navbar-mobile');
+        const navMenu = document.querySelector('.header__navbar-list');
+        const close = document.querySelector('.navbar-mobile-close');
+        const navLink = document.querySelectorAll('.header__navbar-item');
+
+        bars.addEventListener('click', () => {
+            navMenu.classList.toggle('nav-active')
+        });
+
+        close.addEventListener('click', () => {
+            navMenu.classList.remove('nav-active')
+        });
+
+
+        navLink.forEach(n => n.addEventListener('click', () => {
+            navMenu.classList.remove('nav-active')
+        }));
+    }
+    navSlide();
+
+
+// FIX BUG SAFARI
+    $(window).scroll(function () {
+        if ($(document).scrollLeft() != 0) {
+            $(document).scrollLeft(0);
+        }
+    });
